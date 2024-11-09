a311e6aa9a0c26a33da50a20b702a4c5b0e0b72f
“Tristian <tristian.davis@atlasschool.com>
2024-11-09 02:32:51 +0000

    more pep8 app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,15 +10,18 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(error):
     """ 404 Error """
     return make_response(jsonify({'error': "Not found"}), 404)
+
 
 if __name__ == "__main__":
     """ Main Function """
